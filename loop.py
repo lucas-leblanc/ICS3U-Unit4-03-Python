e5cf939d4b72c5657949974e1f947fa5a25173cb
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-11-10 15:49:34 +0000

    added file

--- loop.py
+++ loop.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python3
-
 # Created by: Lucas LeBlanc
 # Created on: Nov 2022
-# This program uses a for loop
+# This program uses for while loops
 
 
 def main():
-    # this function uses a for loop
 
-    # input
-    positive_integer = int(input("Enter how many times to repeat: "))
-    print("")
+    loop_counter = 0
 
-    # process & output
-    for loop_counter in range(positive_integer):
-        print("{0} time through loop.".format(loop_counter))
+    # This function finds the square of all positive integers
+    #  up to the given input
+
+    # Input
+    positive_string = input("Enter an integer >= 0: ")
+
+    # Process and output
+    try:
+        positive_float = float(positive_string)
+        positive_integer = int(positive_float)
+        if positive_integer == positive_float:
+            if positive_integer >= 0:
+                for loop_counter in range(positive_integer + 1):
+                    squared_number = loop_counter * loop_counter
+                    print("{0}² = {1}".format(loop_counter, squared_number))
+            else:
+                print("{0} is not positive.".format(positive_integer))
+        else:
+            print("{0} is not an integer".format(positive_float))
+    except ValueError:
+        print("{0} is not a valid input".format(positive_string))
+    finally:
+        print("\nDone.")
 
 
 if __name__ == "__main__":
     main()
+    
